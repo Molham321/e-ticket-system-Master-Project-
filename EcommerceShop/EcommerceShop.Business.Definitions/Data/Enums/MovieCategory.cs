52d013c73083fe12d094957f685e77c4080bb339
Molham Al khodari <alkhodari@drherwig.de>
2023-10-20 11:25:03 +0200

    add Movie model and MovieCategory enum

---
+++ EcommerceShop/EcommerceShop.Business.Definitions/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceShop.Business.Definitions.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ EcommerceShop/EcommerceShop.Business.Definitions/Movie.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceShop.Business.Definitions.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EcommerceShop.Business.Definitions
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
