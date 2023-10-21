bc2c8709b6072707a954197f91dedccbc6578612
Molham Al khodari <alkhodari@drherwig.de>
2023-10-21 15:46:00 +0200

    config SQL Server

--- EcommerceShop/EcommerceShop.Web/Program.cs
+++ EcommerceShop/EcommerceShop.Web/Program.cs
@@ -1,4 +1,5 @@
 using EcommerceShop.Business.Definitions.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // DbContext configuration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
 
 var app = builder.Build();
 
