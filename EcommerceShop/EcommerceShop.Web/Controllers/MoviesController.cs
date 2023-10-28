4ecadb63b14d843c6aebbc3e8806d0dbe43ddd1b
Molham Al khodari <alkhodari@drherwig.de>
2023-10-28 11:46:49 +0200

    add MoviesController

---
+++ EcommerceShop/EcommerceShop.Web/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using EcommerceShop.Business.Definitions.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceShop.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
