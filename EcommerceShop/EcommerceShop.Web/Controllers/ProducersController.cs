93338cf74558ec92e851952e4a18ea826bff3ddf
Molham Al khodari <alkhodari@drherwig.de>
2023-10-28 11:41:42 +0200

    add ProducersController

---
+++ EcommerceShop/EcommerceShop.Web/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using EcommerceShop.Business.Definitions.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceShop.Web.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
