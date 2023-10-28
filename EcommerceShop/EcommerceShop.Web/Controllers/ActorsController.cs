bed788137d4b44f00581776f5f5d1c6bd2ed8fb5
Molham Al khodari <alkhodari@drherwig.de>
2023-10-28 11:38:19 +0200

    add ActorsController

---
+++ EcommerceShop/EcommerceShop.Web/Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceShop.Business.Definitions.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceShop.Web.Controllers
+{
+    public class ActorsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
