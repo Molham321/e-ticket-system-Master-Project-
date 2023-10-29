5450fc33548c2077c13e256f4fc037cf62162972
Molham Al khodari <alkhodari@drherwig.de>
2023-10-29 13:28:10 +0100

    add IActorsService Interface

---
+++ EcommerceShop/EcommerceShop.Business.Implementations/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿
+using EcommerceShop.Business.Definitions;
+
+namespace EcommerceShop.Business.Implementations
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
