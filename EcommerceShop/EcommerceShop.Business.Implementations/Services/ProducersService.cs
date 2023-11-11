7a6822937563547ea856ec55b13e32848263529f
Molham Al khodari <alkhodari@drherwig.de>
2023-11-11 17:22:23 +0100

    add IProducersService, ProducersService

---
+++ EcommerceShop/EcommerceShop.Business.Implementations/Services/ProducersService.cs
@@ -0,0 +1,14 @@
+﻿
+using EcommerceShop.Business.Definitions;
+using EcommerceShop.Business.Definitions.Data;
+using EcommerceShop.Business.Implementations.Base;
+
+namespace EcommerceShop.Business.Implementations.Services
+{
+    public class ProducersService : EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
