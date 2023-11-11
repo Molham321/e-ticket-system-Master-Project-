e0864e96d03216326eb24402eddc5e5197887fa7
Molham Al khodari <alkhodari@drherwig.de>
2023-11-11 14:37:02 +0100

    add base IEntityBase, IEntityBaseRepository and EntityBaseRepository

---
+++ EcommerceShop/EcommerceShop.Business.Implementations/Base/EntityBaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceShop.Business.Implementations.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EcommerceShop/EcommerceShop.Business.Implementations/Base/IEntityBase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EcommerceShop.Business.Implementations.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
