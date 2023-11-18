a348964a913bfded5a65e3357ec27b0332320ab6
Molham Al khodari <alkhodari@drherwig.de>
2023-11-18 15:25:12 +0100

    add MoviesService

---
+++ EcommerceShop/EcommerceShop.Business.Implementations/Services/MoviesService.cs
@@ -0,0 +1,14 @@
+﻿
+using EcommerceShop.Business.Definitions;
+using EcommerceShop.Business.Definitions.Data;
+using EcommerceShop.Business.Implementations.Base;
+
+namespace EcommerceShop.Business.Implementations.Services
+{
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
+    {
+        public MoviesService(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
