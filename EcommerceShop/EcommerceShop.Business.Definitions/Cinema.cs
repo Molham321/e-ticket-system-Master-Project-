2c789c8a4a3d1f41218be2c83e1070f179b394a2
Molham Al khodari <alkhodari@drherwig.de>
2023-11-18 14:02:20 +0100

    fix cinema Logo in Details, Edit, Delete and Create views add IMoviesService

--- EcommerceShop/EcommerceShop.Business.Definitions/Cinema.cs
+++ EcommerceShop/EcommerceShop.Business.Definitions/Cinema.cs
@@ -8,11 +8,11 @@ namespace EcommerceShop.Business.Definitions
         [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Cinema Logo")]
+        [Display(Name = "Logo")]
         [Required(ErrorMessage = "Cinema Logo is required")]
         public string Logo { get; set; }
 
-        [Display(Name = "Cinema Name")]
+        [Display(Name = "Name")]
         [Required(ErrorMessage = "Cinema Name is required")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Cinema Name must be between 3 and 50 chars")]
         public string Name { get; set; }
