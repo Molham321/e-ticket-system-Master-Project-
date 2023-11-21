4ba5a71b7454d827ea0329adb235b4a71633f2a4
Molham Al khodari <alkhodari@drherwig.de>
2023-11-21 16:43:47 +0100

    add Movie Filter & update Movie View

--- EcommerceShop/EcommerceShop.Web/Controllers/MoviesController.cs
+++ EcommerceShop/EcommerceShop.Web/Controllers/MoviesController.cs
@@ -15,9 +15,23 @@ namespace EcommerceShop.Web.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            //var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             var allMovies = await _service.GetAllAsync(n => n.Cinema);
             return View(allMovies);
+        }
+
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) 
+                || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", allMovies);
         }
     }
 }
