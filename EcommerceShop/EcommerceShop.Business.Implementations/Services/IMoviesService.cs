e7e7d48030b61ed4da9bb60f7bc6bbf32fd8fe9c
Molham Al khodari <alkhodari@drherwig.de>
2023-11-25 15:30:51 +0100

    Edit Movies

--- EcommerceShop/EcommerceShop.Business.Implementations/Services/IMoviesService.cs
+++ EcommerceShop/EcommerceShop.Business.Implementations/Services/IMoviesService.cs
@@ -10,7 +10,7 @@ namespace EcommerceShop.Business.Implementations.Services
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
         Task AddNewMovieAsync(NewMovieVM data);
-
+        Task UpdateMovieAsync(NewMovieVM data);
 
     }
 }
