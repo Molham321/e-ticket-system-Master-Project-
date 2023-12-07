098bdec4f341600b1fa7b529812021dd6ddf4a82
Molham Al khodari <alkhodari@drherwig.de>
2023-12-07 14:08:01 +0100

    add AccountController

---
+++ EcommerceShop/EcommerceShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using EcommerceShop.Business.Definitions.Data;
+using EcommerceShop.Business.Definitions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcommerceShop.Business.Definitions.Data.ViewModels;
+
+namespace EcommerceShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Users()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginVM());
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+    }
+}
