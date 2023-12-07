46608002ef5bc1f0e523f8abbacff704da5d545d
Molham Al khodari <alkhodari@drherwig.de>
2023-12-07 14:33:42 +0100

    add HttpPost Register Logic

--- EcommerceShop/EcommerceShop.Web/Controllers/AccountController.cs
+++ EcommerceShop/EcommerceShop.Web/Controllers/AccountController.cs
@@ -61,5 +61,31 @@ namespace EcommerceShop.Web.Controllers
             return View(new RegisterVM());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
+
     }
 }
