fd4ff379a8ca8c39db04a82e8753cf9afc11e329
Molham Al khodari <alkhodari@drherwig.de>
2023-12-07 14:07:15 +0100

    update AppDbContext: IdentityDbContext

--- EcommerceShop/EcommerceShop.Business.Definitions/Data/AppDbContext.cs
+++ EcommerceShop/EcommerceShop.Business.Definitions/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using EcommerceShop.Business.Definitions.Data.Config;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace EcommerceShop.Business.Definitions.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
