3c19c72f22ff38e4f8061f85a7dc91b44020bc4b
Molham Al khodari <alkhodari@drherwig.de>
2023-12-07 14:29:17 +0100

    add Login HttpPost

--- EcommerceShop/EcommerceShop.Web/Controllers/AccountController.cs
+++ EcommerceShop/EcommerceShop.Web/Controllers/AccountController.cs
@@ -31,6 +31,31 @@ namespace EcommerceShop.Web.Controllers
             return View(new LoginVM());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please, try again!";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(loginVM);
+        }
+
         public IActionResult Register()
         {
             return View(new RegisterVM());
